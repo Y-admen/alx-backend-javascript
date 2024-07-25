52f0cdfe8a4ce64ab5464db470a499e5d18b42f9
Y-admen <yassminash305@gmail.com>
2024-07-25 03:05:48 +0300

    task2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then((response) => {
       console.log('Got a response from the API');
       const status = {
         status: 200,
         body: 'Success',
-		      };
-		      resolve(status);
+      };
+      resolve(status);
     })
-    .catch((errr) => new Error());
+    .catch(() => new Error());
 }
