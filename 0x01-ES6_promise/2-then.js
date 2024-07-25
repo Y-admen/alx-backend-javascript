335047f988e8fdc60f200c316f9a6bed3a040b1b
Y-admen <yassminash305@gmail.com>
2024-07-25 05:49:34 +0300

    task2.2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,11 +3,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      const status = {
+      return {
         status: 200,
-        body: 'Success',
+        body: 'success',
       };
-      return status;
     })
     .catch(() => new Error());
 }
