3fed0504a85565034a24dcb5371198bce997cfa8
Y-admen <yassminash305@gmail.com>
2024-07-27 01:17:50 +0300

    basics

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
-function concatArrays(array1, array2, string) {
+export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
-console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
