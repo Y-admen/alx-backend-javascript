7ab6845cc1109b6f2e0550cb767bbc40cca900a2
Y-admen <yassminash305@gmail.com>
2024-07-28 08:49:35 +0300

    task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,13 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ];
 
   const results = await Promise.allSettled(promises);
 
-  return results.map(result => ({
+  return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
+    value: result.status === 'fulfilled' ? result.value : result.reason,
   }));
 }
