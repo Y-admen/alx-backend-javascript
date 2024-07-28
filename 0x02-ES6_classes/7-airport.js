5a0777221f69d7cca2a250f52ccdcb22094b17b3
Y-admen <yassminash305@gmail.com>
2024-07-28 06:48:37 +0300

    task8

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -12,7 +12,3 @@ export default class Airport {
     return this._code;
   }
 }
-
-const airportSF = new Airport('San Francisco Airport', 'SFO');
-console.log(airportSF);
-console.log(airportSF.toString());
