774f3379b75ed8fa5118253c6ad45603cc616efd
Y-admen <yassminash305@gmail.com>
2024-07-29 21:03:26 +0300

    task2

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,7 @@ export default function getListStudentIds(arr) {
   if (!Array.isArray(arr)) {
     return [];
   }
+  //  creates a new array populated with the results of calling a
+  // provided function on every element in the calling array.
   return arr.map((item) => item.id);
 }
