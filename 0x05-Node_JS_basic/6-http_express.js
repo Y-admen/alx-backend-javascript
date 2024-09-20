0e6853e69b9a3d169e509aee9ebaaea3fcc5bee5
Y-admen <yassminash305@gmail.com>
2024-09-20 06:23:48 +0300

    HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const port = 1245;
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,18 +5,18 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    const databaseFile = process.argv[2];
-    countStudents(databaseFile)
-        .then((output) => {
-            res.send(`This is the list of our students\n${output}`);
-        })
-        .catch((err) => {
-            res.status(500).send(err.message);
-        });
+  const databaseFile = process.argv[2];
+  countStudents(databaseFile)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
 });
 
 app.listen(port);
